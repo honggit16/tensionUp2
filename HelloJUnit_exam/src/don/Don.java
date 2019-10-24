906ea5a56d6e876405023a985bdc1ce80ed5943b
h_pla <h_pla@LAPTOP-0KRBNE8L>
2019-10-24 14:43:49 +0900

    데헷

---
+++ HelloJUnit_exam/src/don/Don.java
@@ -0,0 +1,16 @@
+package don;
+
+class Kaja {
+	public static int kaja(int mom, int daddy) {
+		return mom+daddy;
+			}
+}
+
+public class Don {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//kaja(10000,20000);
+	}
+
+}
---
+++ HelloJUnit_exam/src/don/DonTest.java
@@ -0,0 +1,16 @@
+package don;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DonTest {
+
+
+	@Test
+	void testMain() {
+		//fail("Not yet implemented");
+		assertTrue(Kaja.kaja(10000,20000)==30000);
+	}
+
+}
